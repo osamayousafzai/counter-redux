c022f8372213930c573a1685e412e96aa345738e
Osama Yousafzai <osama.yousafzai@gmail.com>
2021-10-11 20:17:08 +0300

    Updates

---
+++ precache-manifest.984a6b7b81a7b6354c21107fb2fbf65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bea9029619a210b6a2ec91c8d79798",
+    "url": "/counter-redux/index.html"
+  },
+  {
+    "revision": "97d19e0b57625d3cb3ed",
+    "url": "/counter-redux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0ce55e111390b2cdea77",
+    "url": "/counter-redux/static/js/2.4e39e5cd.chunk.js"
+  },
+  {
+    "revision": "97d19e0b57625d3cb3ed",
+    "url": "/counter-redux/static/js/main.754f1666.chunk.js"
+  },
+  {
+    "revision": "b8cdc8059e998f9c0995",
+    "url": "/counter-redux/static/js/runtime~main.f5b2946d.js"
+  }
+]);
